41f95c0ab9990c216c9cbc9686cbe5e6a58fcb34
lucasbrendborg3 <lucas@brendborg.dk>
2026-03-16 11:50:43 +0100

    changes to rviz launch file

--- src/robot_arm_description/launch/rviz.launch.py
+++ src/robot_arm_description/launch/rviz.launch.py
@@ -33,16 +33,8 @@ def generate_launch_description():
         output='screen'
     )
 
-    # This automatically publishes 0.0 for all joints so RViz loads instantly
-    start_joint_state_publisher_cmd = Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher'
-    )
-
     # Return the clean launch description
     return LaunchDescription([
         start_rsp_cmd,
         start_rviz_cmd,
-        #start_joint_state_publisher_cmd
     ])
